01aac7718b39990b0517361530fb5777a6341793
codeline0511 <89986502+codeline0511@users.noreply.github.com>
2024-03-16 20:53:53 +0900

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -7,7 +7,7 @@ def get_circle_area(n):
     value = n * n * 3.14
     return value
 
-a = (input('넓이를 구하고자 하는 원의 반지름은?')
+a = '넓이를 구하고자 하는 원의 반지름은?'
 result = get_radius(a)
 b = get_circle_area(result)
 print('반지름이', result, '인 원의 넓이 = 3.14 *', result,'*', result,'=', b)
